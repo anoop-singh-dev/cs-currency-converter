df122486992c41b4fce6779d9b984e3fee24fdf3
anoopsingh1996 <anoopraghuvansi@gmail.com>
2024-08-02 15:48:21 +0530

    add comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 const freecurrencyapi = new Freecurrencyapi('fca_live_YcZxtffiz0mOj6KwG5uSM1sqMdhEnLIGkXeaNukL');
 
-
+// Convert currency from one unit to other unit
 export async function convertCurrency(fromCurrency, toCurrency, units){
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
